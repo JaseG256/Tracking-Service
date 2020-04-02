b4b68cce9683c99a713e16fbca6d97820243f4ad
JaseG256 <jason_r_gibbs@yahoo.com>
2020-04-02 13:12:19 -0700

    Initial Commit

---
+++ tracking-service/tracking-service/src/test/kotlin/com/msa/trackingservice/TrackingServiceApplicationTests.kt
@@ -0,0 +1,13 @@
+package com.msa.trackingservice
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class TrackingServiceApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
